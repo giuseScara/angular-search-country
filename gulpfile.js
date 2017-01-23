d6032be22842dbad24cc858bfec02f2bba2a113c
Giuseppe <giuse.scara@gmail.com>
2017-01-23 19:47:17 +0100

    Add npm custom script, sourcemap

--- gulpfile.js
+++ gulpfile.js
@@ -13,7 +13,10 @@ var gulp = require('gulp'),
   browserSync = require('browser-sync'),
   del = require('del'),
   ngannotate = require('gulp-ng-annotate'),
-  bower = require("gulp-bower");
+  bower = require("gulp-bower"),
+  sourcemaps = require('gulp-sourcemaps'),
+  imagemin = require('gulp-imagemin');
+
 
 //download libs
 gulp.task('bower', function () {
@@ -33,17 +36,35 @@ gulp.task('clean', function () {
 
 // Default task
 gulp.task('default', ['clean', 'bower'], function () {
-  gulp.start('usemin','copyfonts', 'copyview');
+  gulp.start('usemin', 'imagemin', 'copyfonts', 'copyview');
 });
 
 
 gulp.task('usemin', ['jshint'], function () {
   return gulp.src('./app/index.html')
     .pipe(usemin({
-      css: [minifycss(), rev()],
-      js: [ngannotate(), uglify(), rev()]
+      css: [sourcemaps.init({
+        loadMaps: true
+      }), minifycss(), rev(), sourcemaps.write()],
+      js: [sourcemaps.init({
+        loadMaps: true
+      }), ngannotate(), uglify(), rev(), sourcemaps.write()]
     }))
     .pipe(gulp.dest('dist/'));
+});
+
+// Images
+gulp.task('imagemin', function () {
+  return del(['dist/images']), gulp.src('app/images/**/*')
+    .pipe(cache(imagemin({
+      optimizationLevel: 3,
+      progressive: true,
+      interlaced: true
+    })))
+    .pipe(gulp.dest('dist/images'))
+    .pipe(notify({
+      message: 'Images task complete'
+    })), gulp.src('app/images/favicon.ico').pipe(gulp.dest('dist/images'));
 });
 
 gulp.task('copyview', function () {
@@ -64,6 +85,9 @@ gulp.task('watch', ['browser-sync'], function () {
   gulp.watch('{app/scripts/**/*.js,app/styles/**/*.css,app/**/*.html}', ['usemin']);
 
   gulp.watch('app/view/**/*.*.js', ['usemin']);
+  
+  // Watch image files
+  gulp.watch('app/images/**/*', ['imagemin']);
 
   gulp.watch('app/view/**/*.html', ['copyview']);
 
@@ -71,16 +95,16 @@ gulp.task('watch', ['browser-sync'], function () {
 
 gulp.task('browser-sync', ['default'], function () {
   var files = [
-      'app/**/*.html',
-      'app/styles/**/*.css',
-      'app/images/**/*.png',
-      'app/scripts/**/*.js',
-      'app/view/**/*.js',
-      'app/view/**/*.html',
-      'dist/**/*.*',
-      'dist/**/*.html',
-      'dist/*.html'
-   ];
+    'app/**/*.html',
+    'app/styles/**/*.css',
+    'app/images/**/*.png',
+    'app/scripts/**/*.js',
+    'app/view/**/*.js',
+    'app/view/**/*.html',
+    'dist/**/*.*',
+    'dist/**/*.html',
+    'dist/*.html'
+  ];
 
   setTimeout(function () {
     browserSync.init(files, {
@@ -90,7 +114,7 @@ gulp.task('browser-sync', ['default'], function () {
       }
     });
   }, 3600);
-  
+
   // Watch any files in dist/, reload on change
   gulp.watch(['dist/**']).on('change', browserSync.reload);
 
